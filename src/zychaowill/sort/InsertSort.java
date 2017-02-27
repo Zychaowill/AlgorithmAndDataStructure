21adb61262f0c8bae4089dd1447a0027f7d10177
Zychaowill <15536542139@163.com>
2017-02-27 13:34:34 +0800

    Realizing insert sort algorithm.

--- src/zychaowill/sort/InsertSort.java
+++ src/zychaowill/sort/InsertSort.java
@@ -9,10 +9,14 @@ public class InsertSort {
 	 * 
 	 * @param a
 	 */
-	private <T extends Comparable<T>> void directInsertSort(T[] a) {
-		for (int i = 0; i < a.length; i++) {
+	public <T extends Comparable<T>> void directInsertSort(T[] a) {
+		directInsertSort(a, 0, a.length);
+	}
 
-			for (int j = 0; j < i; j++) {
+	public <T extends Comparable<T>> void directInsertSort(T[] a, int startIndex, int endIndex) {
+		for (int i = startIndex; i < endIndex; i++) {
+
+			for (int j = startIndex; j < i; j++) {
 
 				if (a[i].compareTo(a[j]) < 0) {
 
@@ -29,8 +33,12 @@ public class InsertSort {
 	 * 
 	 * @param a
 	 */
-	private <T extends Comparable<T>> void binaryInsertSort(T[] a) {
-		for (int i = 1; i < a.length; i++) {
+	public <T extends Comparable<T>> void binaryInsertSort(T[] a) {
+		binaryInsertSort(a, 0, a.length);
+	}
+
+	public <T extends Comparable<T>> void binaryInsertSort(T[] a, int startIndex, int endIndex) {
+		for (int i = startIndex + 1; i < endIndex; i++) {
 			T t = a[i];
 			int low = 0, high = i - 1;
 
@@ -54,17 +62,21 @@ public class InsertSort {
 	 * 
 	 * @param a
 	 */
-	private <T extends Comparable<T>> void shellSort(T[] a) {
+	public <T extends Comparable<T>> void shellSort(T[] a) {
+		shellSort(a, 0, a.length);
+	}
+	
+	public <T extends Comparable<T>> void shellSort(T[] a, int startIndex, int endIndex) {
 		int h = 1;
 
-		while (h <= a.length / 3) {
+		while (h <= (endIndex - startIndex) / 3) {
 			h = h * 3 + 1;
 		}
 
 		// To do incremental search and sort.
 		while (h > 0) {
-			for (int i = h; i < a.length; i++) {
-				for (int j = i; j < a.length; j += h) {
+			for (int i = startIndex + h; i < endIndex; i++) {
+				for (int j = i; j < endIndex; j += h) {
 					// Check if need to sort
 					if (a[j].compareTo(a[j - h]) < 0) {
 						T t = a[j];
@@ -83,26 +95,26 @@ public class InsertSort {
 	}
 
 	@Test
-	public void directInsertSortTest() {
+	private void directInsertSortTest() {
 		Integer[] a = new Integer[] { 8, 3, 2, 1, 7, 4, 6, 5 };
 		directInsertSort(a);
 		printArray("直接插入: ", a);
 	}
-	
+
 	@Test
-	public void binaryInsertSortTest() {
+	private void binaryInsertSortTest() {
 		Integer[] a = new Integer[] { 8, 3, 2, 1, 7, 4, 6, 5 };
 		binaryInsertSort(a);
 		printArray("折半插入: ", a);
 	}
-	
+
 	@Test
-	public void shellSortTest() {
+	private void shellSortTest() {
 		Integer[] a = new Integer[] { 8, 3, 2, 1, 7, 4, 6, 5 };
 		shellSort(a);
 		printArray("希尔排序: ", a);
 	}
-	
+
 	private <T extends Comparable<T>> void printArray(String message, T[] a) {
 		System.out.print(message);
 		for (T t : a) {
