babbe3eae8ea80566ad6b924f78bf48915254a54
Zychaowill <15536542139@163.com>
2018-02-04 19:14:32 +0000

    Realizing small head heap

---
+++ src/main/java/zychaowill/datastructure/basic/heap/Main.java
@@ -0,0 +1,24 @@
+package zychaowill.datastructure.basic.heap;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<Integer> numbers = Arrays.asList(3, 5, 10, 7, 9, 15, 11, 13, 20);
+		SmallHeadHeap<Integer> heap = new SmallHeadHeap<>(numbers);
+		String inOrder = heap.inOrder();
+		System.out.println("inOrder: " + inOrder);
+		
+		heap.offer(16);
+		System.out.println("inOrder: " + heap.inOrder());
+		
+		Integer pollE = heap.poll();
+		System.out.println("poll: " + pollE);
+		System.out.println("inOrder: " + heap.inOrder());
+		
+		heap.remove(16);
+		System.out.println("inOrders: " + heap.inOrder());
+	}
+}
---
+++ src/test/java/zychaowill/datastructure/heap/SmallHeadHeapTest.java
@@ -0,0 +1,23 @@
+package zychaowill.datastructure.heap;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SmallHeadHeapTest {
+	
+	@Test
+	public void testSystemMethod() throws Exception {
+		int[] elements = {1, 2, 3, 4, 5, 6, 7};
+		int newLength = (elements.length * 3 >>> 1) + 1;
+		elements = Arrays.copyOf(elements, newLength);
+		String result = Arrays.stream(elements).mapToObj(Integer::new).map(String::valueOf).collect(Collectors.joining(","));
+		log.info("-------------------------------------");
+		log.info("result={}, size={}", result, newLength);
+		log.info("-------------------------------------");
+	}
+}
